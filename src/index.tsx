5861d0272504c2fdd2baa06d74bb5b853f09c9f7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-11-19 07:49:53 +0900

    Use ResourceEditor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import config from './config';
+import { ResourceEditor } from './ResourceEditor';
+import { ResetStyle } from './ResetStyle';
+import { Data } from './Data';
+
+const data = new Data(config);
+
 window.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<div>Hi</div>, window.document.querySelector('#root'));
+  ReactDOM.render(
+    <>
+      <ResetStyle />
+      <ResourceEditor config={config} data={data} />
+    </>,
+    window.document.querySelector('#root'),
+  );
 });
