c3ef8103d89135903a274b9b9d546a97cc165b09
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-11-19 07:49:44 +0900

    Create ResourceTableCell

---
+++ src/ResourceTableCell.tsx
@@ -0,0 +1,148 @@
+import dayjs from 'dayjs';
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { Attribute, SelectOption } from './config';
+import { ResourceRow } from './Data';
+import { Action } from './ResourceEditor';
+
+const Wrapper = styled.td`
+  border: solid 1px #ccc;
+
+  &:first-of-type {
+    border-left: 0;
+  }
+
+  &:last-of-type {
+    border-right: 0;
+  }
+
+  select,
+  input,
+  .checkbox-wrapper {
+    padding: 4px;
+    width: 100%;
+    height: 100%;
+
+    &::placeholder {
+      color: #ccc;
+    }
+  }
+
+  input[type='text'] {
+    text-overflow: ellipsis;
+  }
+
+  input[type='number'] {
+    text-align: right;
+  }
+
+  .checkbox-wrapper {
+    text-align: center;
+
+    input[type='checkbox'] {
+      border: solid 1px #ccc;
+      width: 1rem;
+      height: 1rem;
+      line-height: 1rem;
+      border-radius: 2px;
+
+      &:checked {
+        background: #aaa;
+      }
+    }
+  }
+`;
+
+type ResourceTableCellProps = {
+  resourceName: string;
+  attributeName: string;
+  attribute: Attribute;
+  row: ResourceRow;
+  action: Action;
+};
+
+type ResourceTableCellState = {
+  value: string | number | boolean;
+};
+
+export class ResourceTableCell extends React.Component<ResourceTableCellProps, ResourceTableCellState> {
+  constructor(props: ResourceTableCellProps) {
+    super(props);
+
+    this.state = {
+      value: this.props.row[this.props.attributeName],
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  public onInputChange(event: React.FormEvent<HTMLInputElement | HTMLSelectElement>) {
+    const el = event.currentTarget;
+    const value: string | number | boolean = el.type === 'checkbox' ? (el as HTMLInputElement).checked : el.value;
+    console.log(value);
+    this.setState({ value });
+    this.props.action.update(this.props.resourceName, this.props.row.id, this.props.attributeName, value);
+  }
+
+  public render() {
+    const attribute = this.props.attribute;
+    let content = null;
+
+    switch (attribute.type) {
+      case 'text': {
+        content = (
+          <input type="text" value={String(this.state.value)} placeholder="Text" onChange={this.onInputChange} />
+        );
+        break;
+      }
+      case 'number': {
+        content = <input type="number" value={this.state.value as number} onChange={this.onInputChange} />;
+        break;
+      }
+      case 'boolean': {
+        content = (
+          <div className="checkbox-wrapper">
+            <input type="checkbox" checked={this.state.value as boolean} onChange={this.onInputChange} />
+          </div>
+        );
+        break;
+      }
+      case 'date': {
+        content = (
+          <input
+            type="date"
+            value={dayjs(this.state.value as string).format('YYYY-MM-DD')}
+            onChange={this.onInputChange}
+          />
+        );
+        break;
+      }
+      case 'datetime': {
+        content = (
+          <input
+            type="datetime-local"
+            value={dayjs(this.state.value as string).format('YYYY-MM-DDTHH:mm')}
+            onChange={this.onInputChange}
+          />
+        );
+        break;
+      }
+      case 'select': {
+        const options = attribute.options || [];
+        content = (
+          <select onChange={this.onInputChange}>
+            {options.map((option: SelectOption) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        );
+        break;
+      }
+    }
+
+    return <Wrapper>{content}</Wrapper>;
+  }
+}
