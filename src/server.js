8f30405c47c170325890cc28a2ea1615f52bf6de
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-11-22 09:29:08 +0900

    Create server js

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const express = require('express');
+
+const app = express();
+
+const PATH = './data.json';
+
+app.use(express.json()).use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/resources', (req, res) => {
+  let resources = {};
+
+  if (fs.existsSync(PATH)) {
+    const tmp = fs.readFileSync(PATH);
+    resources = tmp ? JSON.parse(tmp) : {};
+  }
+
+  res.json({ resources });
+});
+
+app.put('/resources', (req, res) => {
+  const data = req.body.data;
+  fs.writeFileSync('./data.json', JSON.stringify(data));
+  res.json({ status: 'success' });
+});
+
+app.listen(3000);
