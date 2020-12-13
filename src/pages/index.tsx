ee851e5844b84cfd58c1cfeb8e45d83c31a64e28
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-14 08:45:43 +0900

    Use Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { ResetStyle } from '../styles/ResetStyle';
+import { Header } from '../components/Header';
 
 export default function IndexPage() {
   // const now = new Date().toLocaleDateString('ja-JP-u-ca-japanese', {
@@ -16,21 +17,18 @@ export default function IndexPage() {
       <ResetStyle />
       <style jsx>{`
         background red;
+
+        .year {
+          background: blue;
+        }
       `}</style>
-      <div>令和2年</div>
-      <div>2020</div>
-      <div>霜月</div>
-      <div>11月</div>
-      <div>23日</div>
-      <div>赤口</div>
-      <div>日</div>
-      <div>勤労感謝の日</div>
-      <div>大寒</div>
-      <div>節分</div>
-      <div>大根</div>
-      <div>ブロッコリー</div>
-      <div>太刀魚</div>
-      <div>柿</div>
+      <Header />
+      <div>
+        <span>大根</span>
+        <span>ブロッコリー</span>
+        <span>太刀魚</span>
+        <span>柿</span>
+      </div>
     </>
   );
 }
