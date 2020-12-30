526dc6271573b8f9fd50e1748b43d620e0a1e238
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-30 22:46:40 +0900

    Update styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,28 +2,29 @@ import * as React from 'react';
 
 export function Header() {
   return (
-    <header>
-      <div className="year-and-date-information">
-        <div className="year">2020年(令和2年)</div>
-        <div className="date-information">
-          <div className="lunar-month-and-month">
-            <div className="lunar-month">霜月</div>
-            <div className="month">11</div>
-          </div>
-          <div className="month-text">月</div>
-          <div className="date">23</div>
-          <div className="date-text">日</div>
+    <header className="py-4 px-2">
+      <div>
+        <div>2020年(令和2年)</div>
+        <div>霜月</div>
+        <div>
+          <div className="inline-block">11</div>
+          <div className="inline-block">月</div>
+          <div className="inline-block">23</div>
+          <div className="inline-block">日</div>
         </div>
-        <div className="day-and-rokuyo">
-          <div className="day">日曜日</div>
-          <div className="rokuyo">赤口</div>
+        <div>
+          <div>日曜日</div>
+          <div>赤口</div>
         </div>
       </div>
 
       <div>
         <div>勤労感謝の日</div>
+        <div>次の祝日は11月23日 日曜日 勤労感謝の日(10日後)</div>
         <div>大寒</div>
+        <div>次の二十四節気は11月23日 日曜日 小寒(10日後)</div>
         <div>節分</div>
+        <div>次の雑期は11月23日 日曜日 小寒(10日後)</div>
       </div>
     </header>
   );
