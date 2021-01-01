c22000c6b8828446958bcf9deb38fd42f3689d70
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-01-01 22:21:51 +0900

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
 
 import { Header } from '../components/Header';
+import { services, DateInfo } from '../services';
 
 export default function IndexPage() {
-  // const now = new Date().toLocaleDateString('ja-JP-u-ca-japanese', {
-  //   era: 'long',
-  //   year: 'numeric',
-  //   month: 'long',
-  //   day: 'numeric',
-  //   weekday: 'short',
-  // });
+  const now = new Date();
+  const [dateInfo, setDateInfo] = React.useState(null);
 
-  return (
+  React.useEffect(() => {
+    services.fetchDate(now).then((dInfo: DateInfo) => {
+      setDateInfo(dInfo);
+    });
+  }, []);
+
+  return dateInfo === null ? (
+    '読み込み中'
+  ) : (
     <div className="max-w-screen-sm mx-auto">
-      <Header />
+      <Header dateInfo={dateInfo} />
       <div>週間カレンダー</div>
       <div>月間カレンダー</div>
       <h2>旬の食べ物</h2>
