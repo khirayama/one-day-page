2ac291e5524b260d29a90d026f5cd0fa73338524
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-01-03 14:33:12 +0900

    Update displaying index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,52 @@ import * as React from 'react';
 import dayjs from 'dayjs';
 import queryString from 'query-string';
 
-import { services, DateInfo } from '../services';
+import { services, DateInfo, ScheduleInfo } from '../services';
 
 export default function IndexPage() {
   let fmt = 'YYYY-MM-DD';
-  let date: string = dayjs().format(fmt);
+
+  let d = dayjs();
   if (typeof window === 'object') {
     const query = queryString.parse(window.location.search);
     if (query.date && typeof query.date === 'string') {
-      date = dayjs(query.date || date).format(fmt);
+      d = dayjs(query.date || d);
     }
   }
+  const date: string = d.format(fmt);
 
   const [dateInfo, setDateInfo] = React.useState(null);
+  const [nextNationalholiday, setNextNationalholiday] = React.useState(null);
+  const [nextSolarterm, setNextSolarterm] = React.useState(null);
+  const [nextSpecialterm, setNextSpecialterm] = React.useState(null);
 
   React.useEffect(() => {
+    const nextD = d.add(1, 'day');
+    // date
     services.fetchDate(date).then((dInfo: DateInfo) => {
       setDateInfo(dInfo);
     });
+    // Next nationalholiday
+    services
+      .fetchSchedules(nextD.format(fmt), d.add(12, 'month').format(fmt), 1, ['nationalholiday'])
+      .then((scheduleInfo: ScheduleInfo[]) => {
+        setNextNationalholiday(scheduleInfo[0]);
+      });
+    // Next solarterm
+    services
+      .fetchSchedules(nextD.format(fmt), d.add(12, 'month').format(fmt), 1, ['solarterm'])
+      .then((scheduleInfo: ScheduleInfo[]) => {
+        setNextSolarterm(scheduleInfo[0]);
+      });
+    // Next specialterm
+    services
+      .fetchSchedules(nextD.format(fmt), d.add(12, 'month').format(fmt), 1, ['specialterm'])
+      .then((scheduleInfo: ScheduleInfo[]) => {
+        setNextSpecialterm(scheduleInfo[0]);
+      });
   }, []);
 
-  return dateInfo === null ? (
+  return dateInfo === null || nextNationalholiday === null || nextSolarterm === null || nextSpecialterm === null ? (
     '読み込み中'
   ) : (
     <div className="max-w-screen-sm mx-auto">
@@ -45,20 +70,20 @@ export default function IndexPage() {
         </div>
 
         <div>
+          {[nextNationalholiday, nextSolarterm, nextSpecialterm].map((scheduleInfo) => {
+            const scheduleDate = dayjs(scheduleInfo.date);
+            console.log(scheduleInfo);
+            return (
+              <div key={scheduleInfo.label + scheduleInfo.date}>
+                次の{scheduleInfo.labelJa}は{scheduleDate.format('M月D日')}({scheduleDate.diff(date, 'day')}日後){' '}
+                {scheduleInfo.name}
+              </div>
+            );
+          })}
           {dateInfo.schedules.map((schedule: DateInfo['schedules'][0]) => {
             return (
               <div key={schedule.name}>
                 {schedule.labelJa} {schedule.name}
-              </div>
-            );
-          })}
-          {Object.keys(dateInfo.nextSchedules).map((scheduleLabel) => {
-            const schedule = dateInfo.nextSchedules[scheduleLabel];
-            const scheduleDate = dayjs(schedule.date);
-            return (
-              <div key={schedule.label + schedule.date}>
-                次の{schedule.labelJa}は{scheduleDate.format('M月D日')}({scheduleDate.diff(date, 'day')}日後){' '}
-                {schedule.name}
               </div>
             );
           })}
