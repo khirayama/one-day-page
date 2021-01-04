15f8ee226d869cca957db9709677773d53a907c1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-01-04 11:12:06 +0900

    Move globals.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import './globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
