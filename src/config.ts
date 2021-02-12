10500c762acbeaac8cb89f3fa5e336148595d055
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-02-13 07:41:31 +0900

    Update config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 export const config = {
   APP_URL: 'https://season-green.vercel.app',
-  // API_SERVER_URL = process.env.NEXT_PUBLIC_API_SERVER_URL || 'http://localhost:4000',
   API_URL: 'https://radiant-badlands-93029.herokuapp.com',
+  // API_URL: 'http://localhost:4000',
+  // API_URL: process.env.NEXT_PUBLIC_API_SERVER_URL || 'http://localhost:4000',
 
   // TODO ちゃんと考える
   lang: 'ja',
