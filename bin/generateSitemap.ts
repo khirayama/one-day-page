259b7c3e2cbfa444a2f8f07831fa7be97d63d71a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-02-22 07:08:05 +0900

    Implement generateSitemap

--- bin/generateSitemap.ts
+++ bin/generateSitemap.ts
@@ -1,5 +1,27 @@
 import * as fs from 'fs';
+import dayjs from 'dayjs';
+
+import { config } from '../src/config';
 
 (() => {
-  fs.writeFileSync('./public/sitemap.xml', 'hi');
+  const now = dayjs();
+  const startDate = dayjs('2020-02-01');
+  const endDate = dayjs('2021-11-31');
+  let current = startDate.clone();
+
+  console.log(`From ${startDate.format('YYYY-MM-DD')} to ${endDate.format('YYYY-MM-DD')}`);
+  const urls = [];
+  while (!current.isAfter(endDate)) {
+    urls.push(`<url>
+<loc>${config.APP_URL}/?date=${current.format('YYYY-MM-DD')}</loc>
+<lastmod>${now.format('YYYY-MM-DD')}</lastmod>
+</url>`);
+    current = current.add(1, 'day');
+  }
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.join('')}
+</urlset>`;
+  fs.writeFileSync('./public/sitemap.xml', xml);
 })();
